d203f1df5c2d4fea1fb512d23b38986bd5f653b0
Felipe <felipeclave@gmail.com>
2025-04-28 10:22:43 -0300

    ciração dos componentes de input e tag

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+import styles from './Tag.module.css'
+
+type Categoria = "2D" | "3D";
+
+type Censura = "Livre" | "10 anos" | "12 anos" | "14 anos" | "16 anos";
+
+interface TagProps {
+    value: Categoria | Censura
+}
+
+const tagClasses = { //objeto que mapeia os estilos para cada classe de css
+    Livre: styles.livre,
+    "10 anos": styles.dez,
+    "12 anos": styles.doze,
+    "14 anos": styles.catorze,
+    "16 anos": styles.dezesseis,
+    "2D": styles.doisD,
+    "3D": styles.tresD,
+};
+
+const Tag = ({ value }: TagProps) => {
+    const classes = classNames(styles.tag, tagClasses[value]) 
+    return (
+    <span className={classes}>{value}</span>
+  )
+};
+
+export default Tag
+
+// os types criam tipos para cada classe de css e o interface junta eles
+//o componente terá os estilos de tag e as classes do valor que for atribuído a ele
