572d8cf6a834cdeb6c0c9dfc2fd200f6f517157d
Felipe <felipeclave@gmail.com>
2025-04-30 08:59:10 -0300

    criação do componente de newsletter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner";
 import Header from "./components/Header";
 import MovieSection from "./components/MovieSection";
+import Newsletter from "./components/Newsletter";
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
       <Header />
       <Banner src="./banner.png" alt="Banner" />
       <MovieSection />
+      <Banner src="./combo.png" alt="Combo" />
+      <Newsletter />
     </>
   )
 }
